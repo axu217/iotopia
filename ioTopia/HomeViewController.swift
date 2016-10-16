f67b158d71bfbfb482d73d21ffb45171f11f9aa1
axu217 <albertxu217@gmail.com>
2016-10-15 18:11:54 -0700

    First commmit

---
+++ ioTopia/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  ioTopia
+//
+//  Created by AXE07 on 10/9/16.
+//  Copyright © 2016 ZheChengXu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+
+}
